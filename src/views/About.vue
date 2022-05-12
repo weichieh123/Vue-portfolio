8c3d29631d0e43438cc929dd2f1ef5e47fa728e7
weichieh123 <weichiehhuang@gapp.nthu.edu.tw>
2022-05-12 14:31:41 +0800

    About

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,20 @@
 <template>
   <div id="about" class="about">
-    <span>This is an about page</span>
+    <h2>Experience</h2>
+    <el-timeline>
+      <el-timeline-item timestamp="2012/9 - 2016/6" placement="top">
+      國立清華大學電機工程學系、攝影社社長(2015年)
+    </el-timeline-item>
+    <el-timeline-item timestamp="2017/12 - 2020/11" placement="top">
+      電子工程師
+    </el-timeline-item>
+    <el-timeline-item timestamp="2021/2 - 2021/7" placement="top">
+      資策會 - 前端就業養成班
+    </el-timeline-item>
+    <el-timeline-item timestamp="2021/8 - Now" placement="top">
+      前端工程師(React、Vue)
+    </el-timeline-item>
+  </el-timeline>
   </div>
 </template>
 
