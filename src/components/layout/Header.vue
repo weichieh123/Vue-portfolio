2e640b1bb1a9a9b9b86a005b37bce94fc8eaec1d
weichieh123 <weichiehhuang@gapp.nthu.edu.tw>
2022-05-12 15:51:28 +0800

    header

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="header">
+  <div class="header" :class="{ active: isOpen }">
     <div class="logo">
       <el-icon><video-camera-filled /></el-icon>
       <span> Weichieh</span>
